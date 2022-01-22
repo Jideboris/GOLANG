d7afa27339c0447b86a4fc802266f801f1cb3506
Tim White <tr.white@outlook.com>
2022-01-22 14:11:00 +0000

    Create user factory & table seeder

--- 1-php/data-capture/app/Models/User.php
+++ 1-php/data-capture/app/Models/User.php
@@ -6,10 +6,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +45,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->id = Str::uuid()->toString();
+        });
+    }
 }
--- 1-php/data-capture/database/seeders/DatabaseSeeder.php
+++ 1-php/data-capture/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
+
+        DB::table('scheduled_questionnaires')->insert([
+            [
+                'id'                => '02f86fb2-763a-4001-b7a0-3f8714e0099d',
+                'questionnaire_id'  => 'e147be89-0116-4aec-8d38-b9af4d51817b',
+                'participant_id'    => $user->id,
+                'scheduled_at'      => '2021-01-03 12:00:00',
+                'status'            => 'complete',
+                'created_at'        => now(),
+                'updated_at'        => now()
+            ]
+        ]);
+        
+        DB::table('questionnaires')->insert([
+            [
+                'id'                        => 'e078de03-b277-4ce0-affd-20297b150e97',
+                'study_id'                  => 'limbs-study',
+                'name'                      => 'Who has legs?',
+                'questions'                 => '{}',
+                'max_attempts'              => 1,
+                'hours_between_attempts'    => 0,
+                'created_at'                => now(),
+                'updated_at'                => now()
+            ]
+        ]);
     }
 }
